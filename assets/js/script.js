a1a9ced8719348bc672ed95cd811216a85074dd8
rosiethesupergeek <rosiethesupergeek.dev@gmail.com>
2023-02-16 12:51:02 +0000

    add code to generate 2 random numbers for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
-function runGame (){
+//wait for the DOM to finish loading before running the game
+// Get the button elements and add listener events to them
 
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+
+});
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+
+
+function runGame() {
+
+    //creates two random numbers between 1-25.
+let num1 = Math.floor(Math.random() * 25)+1;
+let num2 = Math.floor(Math.random() * 25)+1;
 }
 
 function checkAnswer() {
@@ -27,5 +55,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
